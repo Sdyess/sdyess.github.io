a3579f9a6a7cabcb8f9be56f09f2408b3bfd5be7
Skyler Dyess <dyesssp@gmail.com>
2020-03-23 00:08:55 -0500

    Starting on new personal website in Gatsby

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default () => (
+<head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+  <meta name="description" content=""/>
+  <meta name="author" content=""/>
+
+  <title>Skyler Dyess - Software Engineer</title>
+
+  <link href="./static/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet"/>
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet"/>
+  <link href="./static/vendor/fontawesome-free/css/all.min.css" rel="stylesheet"/>
+  <link href="../styles/global.css" rel="stylesheet"/>
+</head>
+) 
