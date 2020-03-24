217765c4997fa05055c1822a03a7ddc6611328d9
Skyler Dyess <dyesssp@gmail.com>
2020-03-24 02:05:51 -0500

    Start rewrite of entire website in React
    New design incoming...

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div class="container">
+                <p>Any opinions expressed here are my own and do not represent the opinons of my employer in any way.</p>
+            </div>
+        )
+    }
+}
+
+export default Footer;
--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,29 @@
-import React from "react"
+import React from 'react';
 
-export default () => (
-<head>
-  <meta charset="utf-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-  <meta name="description" content=""/>
-  <meta name="author" content=""/>
+class Header extends React.Component {
+    render() {
+        return (
+            <div class="container">
+                <img src="./static/images/profile.jpg" alt="Profile" class="rounded-circle" width="120px" height="120px"/>
+                <br/><br/>
+                <h3>Skyler Dyess</h3>
+                <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Social</a>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
 
-  <title>Skyler Dyess - Software Engineer</title>
-
-  <link href="./static/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
-  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet"/>
-  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet"/>
-  <link href="./static/vendor/fontawesome-free/css/all.min.css" rel="stylesheet"/>
-  <link href="./static/global.min.css" rel="stylesheet"/> 
-</head>
-) 
+export default Header;
