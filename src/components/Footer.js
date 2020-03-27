9b51143c59c836675b12dd9217a1672842e8e9a7
Skyler Dyess <dyesssp@gmail.com>
2020-03-27 01:06:59 -0500

    Add name & date to footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-//Footer text will have a year generated in it, so let's keep this as a component
 class Footer extends React.Component {
+    
     render() {
         return (
             <footer class="footer">
                 <div className="container">
-                <p>Any opinions expressed here are my own and do not represent the opinons of my employer in any way.</p>
+                    <p>Skyler Dyess {new Date().getFullYear()} | Any opinions expressed here are my own and do not represent the opinons of my employer in any way.</p>
                 </div>
             </footer>  
         )
