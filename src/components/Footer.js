7b73f234a2a31de8055da6671bb6e8d6a5fc8c13
Skyler Dyess <dyesssp@gmail.com>
2020-03-26 21:04:26 -0500

    Make footer component sticky

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
 import React from "react";
 
+//Footer text will have a year generated in it, so let's keep this as a component
 class Footer extends React.Component {
     render() {
         return (
-            <div className="container">
+            <footer class="footer">
+                <div className="container">
                 <p>Any opinions expressed here are my own and do not represent the opinons of my employer in any way.</p>
-            </div>
+                </div>
+            </footer>  
         )
     }
 }
