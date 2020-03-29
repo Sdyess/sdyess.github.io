492864db3d879369a1ac38e8679058b45e2a7bc5
Skyler Dyess <dyesssp@gmail.com>
2020-03-29 17:36:44 -0500

    Add about me info and remove placeholder text

--- src/components/About.js
+++ src/components/About.js
@@ -6,18 +6,16 @@ class About extends React.Component {
             <div className="container">
                 <h3>Hi, I'm Skyler!</h3>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mauris ligula, bibendum sit amet ex eu, 
-                    pharetra dignissim dui. Nunc sed imperdiet ligula, vitae ultricies justo. Donec vehicula pellentesque ligula. 
-                    Integer sit amet diam finibus, blandit ligula a, pretium enim. Suspendisse semper neque rhoncus mattis lobortis. 
-                    Ut commodo mollis nunc. Donec vehicula, nibh vel eleifend facilisis, purus odio efficitur nunc, nec facilisis enim dui ut enim. 
-                    Phasellus et dolor eu massa aliquet vehicula varius et risus.
+                    I'm a software engineer that currently lives in Austin, Texas. I currently work for The Charles Schwab Corporation creating backend services for mobile
+                    clients as a Mobile Middleware Engineer. Before working at Charles Schwab, I was a full stack web developer for a medium sized distribution company.
+                    I mostly work in the .NET stack, but I am fond of working with lower level programming languages in my free time and enjoy dabbling in languages such 
+                    as Go, (Modern) C++, and Rust. I graduated from Stephen F. Austin State University with a degree in Computer Science.
                 </p>
                 <p>
-                    Integer ultricies pulvinar arcu, vitae viverra magna vestibulum a. Ut tellus dolor, finibus in nisi in, bibendum facilisis sapien. 
-                    Praesent vel mauris at augue volutpat tincidunt sit amet nec est. Proin semper, orci id dapibus ultricies, nunc sem tempor urna, sit 
-                    amet euismod est nibh non neque. In ut pharetra metus, sed luctus est. Nulla elementum maximus tortor in ultrices. Aenean venenatis 
-                    cursus metus nec lacinia. Nulla metus sapien, ultricies consequat pulvinar id, pharetra pellentesque nulla. Fusce tincidunt justo at 
-                    magna hendrerit, eget cursus nibh commodo. 
+                    Some other things I enjoy and/or I am trying to enjoy. I still play World of Warcraft on and off since the original game came out, it is an endless relationship, 
+                    and it is the reason I ever gained an interest in going to school for computer science in the first place. I recently caught up on the books within 
+                    <a href="https://en.wikipedia.org/wiki/The_Kingkiller_Chronicle"> The Kingkiller Chronicle</a> by Patrick Rothfuss. I recently bought some hiking shoes 
+                    thinking I would get into hiking soon since moving to Austin, but it hasn't happened yet. Maybe soon.
                 </p>
                 <br/>
             </div>
